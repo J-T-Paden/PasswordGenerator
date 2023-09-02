f8c438176e47aa631fc985a9c91c214d71ed8506
Jared P <jared.t.paden@gmail.com>
2023-09-02 08:07:20 -0600

    Added each window.prompt for password parameters.

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  lengthPrompt = window.prompt("Please input a length for you password between 8 and 128.");
+  lowercasePrompt = window.prompt("Lowercase characters?");
+  uppercasePrompt = window.prompt("Uppercase characters?");
+  numericPrompt = window.prompt("Numerical characters?");
+  specialPrompt = window.prompt("Special characters?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
