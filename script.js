60eb340183b5fcde8209540e0d57e39e051531d2
Jared P <jared.t.paden@gmail.com>
2023-09-02 08:35:38 -0600

    Added character arrays

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var lowercaseCharacters = [];
+  // Used forloop to create lowercaseCharacters array instead of retyping the alphabet strings
+  for (let i = 0; i < uppercaseCharacters.length; i++) {
+    lowercaseCharacters.push(uppercaseCharacters[i].toLowerCase());
+  };
+  var numericalCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~" ]
   lengthPrompt = window.prompt("Please input a length for you password between 8 and 128.");
   lowercasePrompt = window.prompt("Lowercase characters?");
   uppercasePrompt = window.prompt("Uppercase characters?");
