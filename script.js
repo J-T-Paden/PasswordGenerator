e2103f1d698e92af63c1229cfc9039a508f67c53
Jared P <jared.t.paden@gmail.com>
2023-09-11 21:38:56 -0600

    Added password generation functionality, added boolean values to prompts with logic

--- script.js
+++ script.js
@@ -1,77 +1,101 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
-function writePassword() {
-  // variable declarations
- // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-      passwordText.value = password;
+// variable declarations
+  function generatePassword() {
   var lengthPrompt
   var uppercasePrompt
   var lowercasePrompt
   var numericPrompt
   var specialPrompt
-  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var uppercaseCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
   var lowercaseCharacters = [];
   // Used forloop to create lowercaseCharacters array instead of retyping the alphabet strings
   for (let i = 0; i < uppercaseCharacters.length; i++) {
     lowercaseCharacters.push(uppercaseCharacters[i].toLowerCase());
   };
-  var numericalCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~" ]
-
-// Each random function declaration
-function getRandomLowercase() {
-  return lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];  
-}
-function getRandomUppercase() {
-return uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
-}
-function getRandomNumeric() {
-  return numericalCharacters[Math.floor(Math.random() * numericalCharacters.length)];
-  }
-function getRandomSpecial() {
-  return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-  }
-
+  var numericalCharacters = ["0123456789"];
+  var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~" ]
+  var prePassword
+  // Each random function declaration
   lengthPrompt = window.prompt("Please input a length for your password between 8 and 128 characters.");
   lengthPrompt = Number(lengthPrompt);
     if(lengthPrompt <8 || lengthPrompt >128) {
       window.alert("Password length not within parameters.");
-    } else { password.length = lengthPrompt;
-       // Make password.length = lengthPrompt, move onto lowercaseCharacters
+       // Make prePassword.length = lengthPrompt, move onto lowercaseCharacters
     }
-console.log(password.length);
 
 
     lowercasePrompt = window.confirm("Lowercase characters?");
     if(lowercasePrompt == true) {
-      // populate password with lowercaseCharacters randomly
-    } else {
-      // do not populate password with lowercaseCharacters, move onto uppercaseCharacters
-    }
-    uppercasePrompt = window.confirm("Uppercase characters");
-    if(uppercasePrompt == true) {
-      // populate password with uppercaseCharacters randomly
-    } else {
-      // do not populate password with uppercaseCharacters, move onto numericalPrompt
-    }
+      prePassword = +lowercaseCharacters
+    } else {prePassword = ""}
+
+    uppercasePrompt = window.confirm("Uppercase characters?");
+    if(uppercasePrompt == true && lowercasePrompt == true) {
+      prePassword = prePassword +uppercaseCharacters +lowercaseCharacters
+    } else if (uppercasePrompt == true && lowercasePrompt == false){
+      prePassword = prePassword +uppercaseCharacters
+    } else if (uppercasePrompt == false && lowercasePrompt == true) {
+      prePassword = prePassword +lowercaseCharacters
+    } else {prePassword = ""};
+
     numericPrompt = window.confirm("Numeric characters?");
-    if(numericPrompt == true) {
-      // populate password with numericalCharacters randomly
-    } else {
-      // do not populate password with numericalCharacters move onto specialPrompt
-    }
+    if(uppercasePrompt == true && lowercasePrompt == true && numericPrompt == true) {
+      prePassword = prePassword +uppercaseCharacters +lowercaseCharacters +numericalCharacters
+    } else if(uppercasePrompt == false && lowercasePrompt == true && numericPrompt == true) {
+      prePassword = prePassword +lowercaseCharacters +numericalCharacters
+    } else if (uppercasePrompt == false && lowercasePrompt == false && numericPrompt == true) {
+      prePassword = prePassword +numericalCharacters
+    } else {prePassword = ""};
+
     specialPrompt = window.confirm("Special characters?");
-    if(specialPrompt == true) {
-      // populate password with speccialCharacters randomly
-    } else {
-      // do not populate password with specialCharacters, give password
+    // if all prompts return true
+    if(uppercasePrompt == true && lowercasePrompt == true && numericPrompt == true && specialPrompt == true) {
+      prePassword = prePassword +uppercaseCharacters +lowercaseCharacters +numericalCharacters +specialCharacters
+      // each iteration if 1 prompt returns false
+    } else if(uppercasePrompt == false && lowercasePrompt == true && numericPrompt == true && specialPrompt == true) {
+      prePassword = prePassword +lowercaseCharacters +numericalCharacters +specialCharacters
+    } else if(uppercasePrompt == true && lowercasePrompt == false && numericPrompt == true && specialPrompt == true) {
+      prePassword = prePassword +uppercaseCharacters +numericalCharacters +specialCharacters
+    } else if(uppercasePrompt = true && lowercasePrompt == true && numericPrompt == false && specialCharacters == true) {
+      prePassword = prePassword +uppercaseCharacters +lowercaseCharacters +specialCharacters
+    } else if(uppercasePrompt == true && lowercasePrompt == true && numericPrompt == true && specialPrompt == false) {
+      prePassword = prePassword +uppercaseCharacters +lowercaseCharacters +numericalCharacters
+      // each iteration if 2 prompts return false
+    } else if (uppercasePrompt == false && lowercasePrompt == false && numericPrompt == true && specialPrompt == true) {
+      prePassword = prePassword +numericalCharacters +specialCharacters
+    } else if (uppercasePrompt == true && lowercasePrompt == false && numericPrompt == false && specialPrompt == true) {
+      prePassword = prePassword +uppercaseCharacters +specialCharacters
+    } else if (uppercasePrompt == true && lowercasePrompt == true && numericPrompt == false && specialPrompt == false) {
+      prePassword = prePassword +uppercaseCharacters +lowercaseCharacters
+    } else if (uppercasePrompt == false && lowercasePrompt == true && numericPrompt == true && specialPrompt == false) {
+      prePassword = prePassword +lowercaseCharacters +numericalCharacters
+      // each iterration if 3 prompts return false
+    } else if (uppercasePrompt == false && lowercasePrompt == false && numericPrompt == false && specialPrompt == true) {
+      prePassword = prePassword +specialCharacters
+    } else if (uppercasePrompt == true && lowercasePrompt == false && numericPrompt == false && specialPrompt == false) {
+      prePassword = prePassword +uppercaseCharacters
+    } else if (uppercasePrompt == false && lowercasePrompt == true && numericPrompt == false && specialPrompt == false) {
+      prePassword = prePassword +lowercaseCharacters
+    } else if (uppercasePrompt == false && lowercasePrompt == false && numericPrompt == true && specialPrompt == false) {
+      prePassword = prePassword +numericalCharacters
+    } else {prePassword = ""};
+    // if all prompts return false, return alert
+    if (uppercasePrompt == false && lowercasePrompt == false && numericPrompt == false && specialPrompt == false) {
+      window.alert("No character type selected. Unable to create password.")
     }
-
-  
+    console.log(prePassword);
+    prePassword.length = lengthPrompt;
+    for (let i = 0; i < prePassword.length; i++) {
+      password += prePassword[[Math.floor(Math.random() * prePassword.length)]];
+    };
+  return password;
 }
-
 // Add event listener to generate button
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+}
 generateBtn.addEventListener("click", writePassword);
